c667990039a5dcdc86d52135687032ce04595589
Phireh <Phireh@hotmail.com>
2019-06-22 03:10:57 +0200

    Added support for arbitrary amount of folders and subfolders, added auxiliary debug messages and some more regex variables that need to be kept updated. Changed some variable names and version number.

--- lib/reader.py
+++ lib/reader.py
@@ -1,4 +1,5 @@
 import re
+import os
 from lib.comment import Comment
 
 #   this is the module to read the files and find the comments in them
@@ -9,9 +10,18 @@ from lib.comment import Comment
 POUNDSIGN_REGEX = re.compile(r"#\s*(TODO.*)", re.IGNORECASE)
 DOUBLESLASH_REGEX = re.compile(r"//\s*(TODO.*)", re.IGNORECASE) # Not used yet
 SINGLESLASH_REGEX = re.compile(r"/\*\s*(TODO.*)\s*\*/", re.IGNORECASE)
+
+# plain text regexes for filename extensions
+PYTHON_REGEX_RAW = r".*\.py$"
+C_REGEX_RAW = r".*\.([ch]|cpp)$"
+
 # TODO: use case-sensitive filename regexes in Linux / Mac
-PYTHON_REGEX = re.compile(r".*\.py", re.IGNORECASE)
-C_REGEX = re.compile(r".*\.([ch]|cpp)", re.IGNORECASE)
+PYTHON_REGEX = re.compile(PYTHON_REGEX_RAW, re.IGNORECASE)
+C_REGEX = re.compile(C_REGEX_RAW, re.IGNORECASE)
+
+# used for checking that a filename is even valid to process, make sure it contains all the above file extensions
+# also make sure all "|" are inside parenthesis for proper combining
+ALL_EXTENSIONS_REGEX = re.compile("|".join([PYTHON_REGEX_RAW, C_REGEX_RAW]), re.IGNORECASE)
 
 def get_regex_by_filename(fname):
     result = []
@@ -32,20 +42,36 @@ def get_regex_by_filename(fname):
 
 def read_files(files_to_read):
     comments = []
-    print(files_to_read)
-    if files_to_read.isFolder:
-        #TODO:  add for folder
-        return comments
-    else:
-        for fname in files_to_read.names:
-            linenum = 0
-            file = open(fname, "r")
-            regex_list = get_regex_by_filename(fname)
-            for line in file:
-                linenum += 1
-                for regex in regex_list:
-                    match = re.search(regex, line)
-                    if match:
-                        comments.append(Comment(fname, linenum, match.group(1)))
-            file.close()
+
+    # Get correct filenames if we got the dir name instead
+    if files_to_read.is_folder:
+        folders = files_to_read.names
+        files = []
+        if (files_to_read.debug_mode):
+            print("Files found:")
+        for f in folders:
+            fnames = [os.path.join(r,file) for r,d,f in os.walk(f) for file in f]
+            # Only add known source code extensions to the list
+            for fname in fnames:
+                if (files_to_read.debug_mode):
+                    print(fname)
+                match = re.search(ALL_EXTENSIONS_REGEX, fname)
+                if match:
+                    files.append(fname)
+        files_to_read.names = files
+        if (files_to_read.debug_mode):
+            print(files_to_read.names)
+
+    # Normal processing
+    for fname in files_to_read.names:
+        linenum = 0
+        file = open(fname, "r")
+        regex_list = get_regex_by_filename(fname)
+        for line in file:
+            linenum += 1
+            for regex in regex_list:
+                match = re.search(regex, line)
+                if match:
+                    comments.append(Comment(fname, linenum, match.group(1)))
+        file.close()
     return comments
