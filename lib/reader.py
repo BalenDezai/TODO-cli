2f2423540406eea15a29d5cfa13a3535eb28fd72
Phireh <Phireh@hotmail.com>
2019-07-12 20:04:57 +0200

    Added config file and extension validation, and cleaned up reader.py code

--- lib/reader.py
+++ lib/reader.py
@@ -1,38 +1,17 @@
 import re
 import os
 from lib.comment import Comment
+from lib.config import lang_list
 
 #   this is the module to read the files and find the comments in them
-
-
-# REGEX constants to find the comment
-# TODO: support for multiline comments
-POUNDSIGN_REGEX = re.compile(r"#\s*(TODO.*)", re.IGNORECASE)
-DOUBLESLASH_REGEX = re.compile(r"//\s*(TODO.*)", re.IGNORECASE) # Not used yet
-SINGLESLASH_REGEX = re.compile(r"/\*\s*(TODO.*)\s*\*/", re.IGNORECASE)
-
-# Specific file extension constant
-PYTHON_EXT = '.py'
-
-
-def get_regex_list_by_filename(fname:str):
-    regex_list = []
-    # chech for Python extension
-    if fname.lower().endswith(PYTHON_EXT):
-        regex_list.append(POUNDSIGN_REGEX)
-        return regex_list
-    # TODO: more file extensions
-    else:
-        regex_list.append(DOUBLESLASH_REGEX)
-        regex_list.append(SINGLESLASH_REGEX)
-        return regex_list
 
 def read_comments_in_files(file_names):
     found_comments = []
     for fname in file_names:
         linenum = 0
         file = open(fname, "r")
-        regex_list = get_regex_list_by_filename(fname)
+        file_extension = os.path.splitext(fname)[1].lower()
+        regex_list = lang_list[file_extension].get_compiled_regexes()
         for line in file:
             linenum += 1
             for regex in regex_list:
@@ -66,9 +45,6 @@ def read_files(files_to_read):
             for fname in fnames:
                 if (files_to_read.debug_mode):
                     print(fname)
-                #match = re.search(ALL_EXTENSIONS_REGEX, fname)
-                #if match:
-                    #files.append(fname)
                 if (fname.lower().endswith(tuple(files_to_read.extensions))):
                     files.append(fname)
         files_to_read.names = files
--- todo_cli.py
+++ todo_cli.py
@@ -2,6 +2,7 @@ import sys
 from lib import commands
 from lib import reader
 from lib import writer
+from lib import config
 
 
 # TODO: Not really a TODO, I'm just testing that the program works on itself
@@ -9,11 +10,22 @@ from lib import writer
 def main():
     # TODO: Use '-' to read from stdin instead
     argument_obj = commands.command_interpreter()
-    if argument_obj.new_config == True:
-        print('hey!')
-    else:
-        comments = reader.read_files(argument_obj)
-        writer.print_out(comments)
+
+    # Check that language extensions are defined in config file
+    error = False
+    for extension in argument_obj.extensions:
+        try:
+            config.lang_list[extension]
+        except KeyError:
+            print("ERROR:\t\"" + extension + "\" is not a recognized extension in the config file")
+            error = True
+
+    if error:
+        return
+
+    # Call the reader if all is good 
+    comments = reader.read_files(argument_obj)
+    writer.print_out(comments)
 
 
 if __name__ == "__main__":
