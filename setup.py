dcb7cc6223c1545ceceaf3a2060cf4a1a311486d
BalenD <balen1996@hotmail.com>
2019-08-25 01:32:38 +0200

    added manifest.in and setup.cfg

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     entry_points = {
         'console_scripts': ['todo-cli=todocli.todo_cli:main'],
     },
+    install_requires=[
+        'colorama',
+    ],
     author_email="Balen1996@hotmail.com",
     description="CLI that shows all TODO comments in your files",
     long_description=long_description,
@@ -18,6 +21,10 @@ setup(
     url="https://github.com/BalenD/TODO-cli",
     license='MIT',
     packages=find_packages(),
+    project_urls={
+        'Documentation': 'https://github.com/BalenD/TODO-cli',
+        'Issue Tracker': 'https://github.com/BalenD/TODO-cli/issues',
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
